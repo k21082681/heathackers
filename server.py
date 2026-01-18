5da48eb52d1977eb41c711691111a6f949321bd7
Shahad Alhamazani <shahadalhamazani@Shahads-MacBook-Air.local>
2026-01-18 14:16:31 +0300

    Fix bugs

--- server.py
+++ server.py
@@ -112,6 +112,8 @@ async def load_models():
 
     with open(winner_file, "r") as f:
         WINNER = f.read().strip()
+        if WINNER == "rf":
+            WINNER = "Random Forest"
     print(f"✓ Model winner: {WINNER.upper()}")
 
     for target in TARGETS:
